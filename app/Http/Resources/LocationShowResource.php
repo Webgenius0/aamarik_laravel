cc6cd43579cb9c4bc4a2cf0365ba42a72e9bcf37
ArifurDev <arifurrahmanrifat72@gmail.com>
2024-12-20 15:13:03 +0600

    feat(LocationShowResource): add wishlist check logic
    Added logic to check if the location is in the user's wishlist in the LocationShowResource.
    The check is performed by querying the user's wishlists using the location ID.
    The result is returned as 'yes' or 'no' based on the presence of the location in the wishlist.

--- app/Http/Resources/LocationShowResource.php
+++ app/Http/Resources/LocationShowResource.php
@@ -28,6 +28,19 @@ class LocationShowResource extends JsonResource
             'map_url'      => $this->map_url,
             'points'       => $this->points,
             'puzzle_image' => $this->puzzle_image,
+            'is_wishlist'  => $this->is_wishlist($this->id),
         ];
+    }
+
+
+    /**
+     * Check if the location is in the wishlist
+     */
+    private function is_wishlist($locationID)
+    {
+        //get currect auth user
+        $user = auth()->user();
+        $is_wishlist = $user->wishlists->where('location_id', $locationID)->first();
+        return $is_wishlist ? 'yes' : 'no';
     }
 }
