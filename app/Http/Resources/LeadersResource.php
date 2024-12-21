3f79cf3281d5f90d1e7c1bcedbab82895bddd618
ArifurDev <arifurrahmanrifat72@gmail.com>
2024-12-21 11:12:16 +0600

    feat: update level calculation logic based on completed group puzzles
    - Enhanced level calculation by checking if the leader has completed exactly 9 puzzles in a group to increase their level.
    - Iterated through all groups the leader is part of and counted the completed puzzles.
    - Increased leader's level for each group where exactly 9 puzzles were completed.

--- app/Http/Resources/LeadersResource.php
+++ app/Http/Resources/LeadersResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\LocationReach;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -21,7 +22,7 @@ class LeadersResource extends JsonResource
             'name'   => $this->name,
             'avatar' => $this->avatar,
             'points' => $this->points,
-            'level'  => '1',
+            'level'  => $this->levelCalculation($this->id),
             'rank'   => $this->rankCalculation(),
         ];
     }
@@ -41,5 +42,34 @@ class LeadersResource extends JsonResource
 
         // Return rank as a 1-based index
         return $user_rank !== false ? $user_rank + 1 : null;
+    }
+
+
+    /**
+     * calculate level of the leader based on complete group puzzle
+     */
+    private function levelCalculation($leaderID)
+    {
+        // Initialize level to 0 (no level)
+        $level = 0;
+
+        // Retrieve all group IDs for the leader
+        $groupsID = LocationReach::where('user_id', $leaderID)
+            ->groupBy('group_id')
+            ->get(['group_id']);
+
+        // Iterate through each group
+        foreach ($groupsID as $groupID) {
+            // Count how many LocationReach entries exist for the current group_id
+            $groupPuzzles = LocationReach::where('user_id', $leaderID)
+                ->where('group_id', $groupID->group_id)
+                ->count();
+
+            // If the user has completed exactly 9 puzzles in the group, increase level
+            if ($groupPuzzles == 9) {
+                $level++;
+            }
+        }
+        return $level; // Return the calculated level
     }
 }
