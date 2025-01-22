37a9d878f3ede87a43211d52d6d57f2994e132e0
ArifurDev <arifurrahmanrifat72@gmail.com>
2025-01-22 11:31:17 +0600

    fix: refactor TreatmentAssessmentResource to use options array for assessments

--- app/Http/Resources/TreatmentAssessmentResource.php
+++ app/Http/Resources/TreatmentAssessmentResource.php
@@ -15,19 +15,47 @@ class TreatmentAssessmentResource extends JsonResource
     public function toArray(Request $request): array
     {
 //        return parent::toArray($request);
+//        return [
+//            'id' => $this->id,
+//            'name' => $this->name,
+//            'assessments' => $this->assessments->map(function ($assessment) {
+//                return [
+//                    'id' => $assessment->id,
+//                    'question' => $assessment->question,
+//                    'option1'  => $assessment->option1,
+//                    'option2'  => $assessment->option2,
+//                    'option3'  => $assessment->option3,
+//                    'option4'  => $assessment->option4,
+//                    'answer'   => $assessment->answer,
+//                    'note'     => $assessment->note,
+//                ];
+//            })
+//        ];
         return [
             'id' => $this->id,
             'name' => $this->name,
             'assessments' => $this->assessments->map(function ($assessment) {
+                $options = [];
+
+                if ($assessment->option1) {
+                    $options[] = ['id' => 1, 'value' => $assessment->option1];
+                }
+                if ($assessment->option2) {
+                    $options[] = ['id' => 2, 'value' => $assessment->option2];
+                }
+                if ($assessment->option3) {
+                    $options[] = ['id' => 3, 'value' => $assessment->option3];
+                }
+                if ($assessment->option4) {
+                    $options[] = ['id' => 4, 'value' => $assessment->option4];
+                }
+
                 return [
                     'id' => $assessment->id,
                     'question' => $assessment->question,
-                    'option1'  => $assessment->option1,
-                    'option2'  => $assessment->option2,
-                    'option3'  => $assessment->option3,
-                    'option4'  => $assessment->option4,
-                    'answer'   => $assessment->answer,
-                    'note'     => $assessment->note,
+                    'options' => $options,
+                    'answer' => $assessment->answer,
+                    'note' => $assessment->note,
                 ];
             })
         ];
