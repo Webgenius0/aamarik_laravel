1f7aeac45b990e0a443bd173c62c031c7a4108b3
ArifurDev <arifurrahmanrifat72@gmail.com>
2025-01-26 11:35:10 +0600

    feat: add billing details to StripeCardResource response

--- app/Http/Resources/StripeCardResource.php
+++ app/Http/Resources/StripeCardResource.php
@@ -16,12 +16,17 @@ class StripeCardResource extends JsonResource
     {
 //        return parent::toArray($request);
         return [
+
             'id'        => $this['id'] ?? null,
             'brand'     => $this['card']['brand'] ?? null,
             'last4'     => $this['card']['last4'] ?? null,
             'exp_month' => $this['card']['exp_month'] ?? null,
             'exp_year'  => $this['card']['exp_year'] ?? null,
             'default'   => $this->default ?? false,
+            'billing_details' => [
+                'name'  => $this['billing_details']['name'] ?? null,
+                'email' => $this['billing_details']['email'] ?? null,
+            ],
         ];
     }
 }
